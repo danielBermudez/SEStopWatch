db8e0685e9f8c7ecb064fc69dc5338f4b4bb5d14
Daniel Bermúdez <daniel.bermudez@endava.com>
2019-03-05 10:22:49 -0500

    First Assginment Complete

--- Stopwatch-lesson01/Stopwatch/ViewController.swift
+++ Stopwatch-lesson01/Stopwatch/ViewController.swift
@@ -12,6 +12,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setLabel()
+        setStart()
+        setStop()
+     
+    }
+    func setStart(){
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+        button.center = CGPoint(x: view.center.x - (view.center.x * 0.3), y: 600)
+        button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        button.setTitle("Start", for: .normal)
+        self.view.addSubview(button)
+    }
+        func setStop(){
+            let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+            button.center = CGPoint(x: (view.center.x + (view.center.x * 0.3)), y: 600)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            button.setTitleColor(UIColor.blue, for: .normal)
+            button.setTitle("Stop", for: .normal)
+            self.view.addSubview(button)
+        }
+    func setLabel(){
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 250, height: 41))
+        label.center = CGPoint(x: view.center.x, y: 250)
+        label.textAlignment = NSTextAlignment.center
+        label.text = "0:00.0"
+        label.font = UIFont(name: label.font.fontName, size: 40)
+        self.view.addSubview(label)
     }
     
     override func didReceiveMemoryWarning() {
