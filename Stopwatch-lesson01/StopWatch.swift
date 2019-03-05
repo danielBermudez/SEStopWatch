2ea6a6ae570db4e0df6425fea6181e34c005f797
Daniel Bermúdez <daniel.bermudez@endava.com>
2019-03-05 10:58:33 -0500

    Lesson02 Completed

---
+++ Stopwatch-lesson01/StopWatch.swift
@@ -0,0 +1,26 @@
+//
+//  StopWatch.swift
+//  Stopwatch
+//
+//  Created by Daniel Bermudez on 3/5/19.
+//  Copyright © 2019 Your School. All rights reserved.
+//
+
+import Foundation
+class Stopwatch{
+    private var startTime : NSDate?
+    var elapsedTime : TimeInterval{
+        if let startTime = self.startTime{
+            return -startTime.timeIntervalSinceNow
+        }else {
+            return 0
+        }
+        
+    }
+    func start(){
+        startTime = NSDate()
+    }
+    func stop(){
+        startTime = nil
+    }
+}
